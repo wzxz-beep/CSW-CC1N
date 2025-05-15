bab10771ad7b9844bee6008aa47ad214b18d98e3
wzxz-beep <willianzxz035@gmail.com>
2025-05-14 22:33:51 -0300

    Add files via upload

---
+++ AULA 18/AULA 18/script.js
@@ -0,0 +1,18 @@
+
+
+document.write("<h2> olá mundo! Esse é meu primeiro programa javaScript. </h2>");
+document.write("por. Fabricio é o cara!!");
+window.alert("olá mundo! Esse é meu primeiro programa javaScript.");
+
+function abrepagina()
+{
+    window.open("aula 18.html", "_blank");
+}
+
+<script type="text/javascript">
+    document.getElementById("subtitulo").innerHTML = "Esse é meu segundo programa javaScript.";
+/*document.getElementById("resposta").innerHTML = window.confirm("voce está gostando do curso?");*/
+
+    document.getElementById("resposta").innerHTML = "o nome digitado foi:"
+</script>
+
